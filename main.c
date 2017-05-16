68b6f46bb8fd0e4f56c3a1aeefcd27349849b1a6
Isaac Kennedy Diniz Gomes <isaken@A304-05L.imd.local>
2017-05-16 10:31:25 -0300

    Began setting the arguments

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv){
+	int eflag = 0;
+	int iflag = 0;
+	char *ivalue = NULL;
+	int fflag = 0;
+	char *fvalue = NULL;
+	int hflag = 0;
+	char *hvalue = NULL;
+	int index;
+	int c;
+	
+	int opterr = 0;
+	
+	while ((c = getopt (argc, argv, "ei:f:h:"))) {
+	
+	}
+	
+
+	return 0;
+}
